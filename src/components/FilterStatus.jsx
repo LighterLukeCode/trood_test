618b5c1504f996d9c9dbff2f499577fb9ad50c8f
LighterLukeCode <lighterluke@gmail.com>
2022-09-28 23:03:58 +0300

    add filter

---
+++ src/components/FilterStatus.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Filter = ({ setFilterValue, filterValue, filterBy }) => {
+  const filters = {
+    status: ["All", "green", "yellow", "red"],
+    type: ["All", "TRST", "THT", "THC", "TRST"],
+  };
+  console.log(filters[filterBy]);
+
+  //dataRows.filter(el => el.status === currentStatus && el.type === currentType)
+  const toggleStatus = e => {
+    setFilterValue(e.target.value);
+  };
+
+  return (
+    <div>
+      <select value={filterValue} onChange={toggleStatus}>
+        {filters[filterBy].map((status, i) => (
+          <option value={status} key={i}>
+            {status}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default Filter;
--- src/components/table/index.jsx
+++ src/components/table/index.jsx
@@ -1,10 +1,19 @@
+import React from "react";
 import data from "../../data.json";
+import Filter from "../FilterStatus";
 const Table = () => {
+  const [statusFilter, setStatusFilter] = React.useState("All");
+  const [typeFilter, setTypeFilter] = React.useState("All");
+
   return (
     <table>
       <tr>
-        <th>All Projects</th>
-        <th>All Token type</th>
+        <th>
+          <Filter setFilterValue={setStatusFilter} filterValue={statusFilter} filterBy={"status"} /> Projects
+        </th>
+        <th>
+          <Filter setFilterValue={setTypeFilter} filterValue={typeFilter} filterBy={"type"} /> Token type
+        </th>
         <th>Conditions</th>
         <th>Volume</th>
         <th>ROI</th>
@@ -12,20 +21,29 @@ const Table = () => {
         <th>Isurence hedge</th>
         <th></th>
       </tr>
-      {data.map(token => (
-        <tr key={token.id}>
-          <td>{token.name}</td>
-          <td>{token.type}</td>
-          <td>{token.conditions}</td>
-          <td>{token.volume}</td>
-          <td>{token.roi}</td>
-          <td>{token.free}</td>
-          <td>{token.hedge}</td>
-          <td>
-            <button>buy</button>
-          </td>
-        </tr>
-      ))}
+      {data
+        .filter(
+          token =>
+            (statusFilter === "All" || token.status === statusFilter) &&
+            (typeFilter === "All" || token.type === typeFilter)
+        )
+        .map(token => (
+          <tr key={token.id}>
+            <td>
+              {token.status}
+              {token.name}
+            </td>
+            <td>{token.type}</td>
+            <td>{token.conditions}</td>
+            <td>{token.volume}</td>
+            <td>{token.roi}</td>
+            <td>{token.free}</td>
+            <td>{token.hedge}</td>
+            <td>
+              <button>buy</button>
+            </td>
+          </tr>
+        ))}
     </table>
   );
 };
