23974f3b6e0074ee901af3b17b198061c6505569
LighterLukeCode <lighterluke@gmail.com>
2022-10-04 20:47:18 +0300

    add circle status

--- src/components/table/index.jsx
+++ src/components/table/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styles from "./table.module.scss";
 
 const Table = ({ onSort, filters, onFilter, items, onBuy }) => {
   const onChangeFilter = e => {
@@ -13,11 +14,24 @@ const Table = ({ onSort, filters, onFilter, items, onBuy }) => {
     onBuy(id);
   };
 
+  const backGround = token => {
+    let backColor;
+    if (token.status === "green") {
+      backColor = "rgba(187, 247, 193, 0.8)";
+      console.log(backColor);
+      return backColor;
+    } else if (token.status === "yellow") {
+      backColor = "rgba(247, 246, 187, 0.8)";
+      return backColor;
+    } else backColor = "rgba(247, 187, 193, 0.8)";
+    return backColor;
+  };
+
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>
+    <table className={styles.table}>
+      <thead className={styles.thead}>
+        <tr className={styles.tr}>
+          <th className={styles.th}>
             <select data-filter="status" onChange={e => onChangeFilter(e)}>
               <option value="All">All</option>
               <option value="green">green</option>
@@ -26,7 +40,7 @@ const Table = ({ onSort, filters, onFilter, items, onBuy }) => {
             </select>
             <label onClick={() => onSort("name")}>Projects</label>
           </th>
-          <th>
+          <th className={styles.th}>
             <select data-filter="type" onChange={e => onChangeFilter(e)}>
               <option value="All">All</option>
               <option value="THT">THT</option>
@@ -35,40 +49,40 @@ const Table = ({ onSort, filters, onFilter, items, onBuy }) => {
             </select>
             <label onClick={() => onSort("type")}>Token type</label>
           </th>
-          <th>
+          <th className={styles.th}>
             <label onClick={() => onSort("conditions")}>Conditions</label>
           </th>
-          <th>
+          <th className={styles.th}>
             <label onClick={() => onSort("volume")}>Volume</label>
           </th>
-          <th>
+          <th className={styles.th}>
             <label onClick={() => onSort("roi")}>ROI</label>
           </th>
-          <th>
+          <th className={styles.th}>
             <label onClick={() => onSort("free")}>Free float</label>
           </th>
-          <th>
+          <th className={styles.th}>
             <label onClick={() => onSort("hedge")}>Insurence hedge</label>
           </th>
-          <th></th>
+          <th className={styles.th}></th>
         </tr>
       </thead>
-      <tbody>
+      <tbody className={styles.tbody}>
         {items.map(token => (
-          <tr key={token.id}>
-            <td>
+          <tr className={styles.tr} key={token.id}>
+            <td style={{ backgroundColor: backGround(token) }} className={styles.td}>
               <Link to={`/projects/${token.id}`}>
-                {token.status}
+                <div className={`${styles.sphere} ${styles[token.status]}`}></div>
                 {token.name}
               </Link>
             </td>
-            <td>
+            <td style={{ backgroundColor: backGround(token) }} className={styles.td}>
               <Link to={`/projects/${token.id}`}>{token.type}</Link>
             </td>
-            <td>
+            <td style={{ backgroundColor: backGround(token) }} className={styles.td}>
               <Link to={`/projects/${token.id}`}>{token.conditions.replace("x", "x ").replace(",", ", ")}</Link>
             </td>
-            <td>
+            <td style={{ backgroundColor: backGround(token) }} className={styles.td}>
               <Link to={`/projects/${token.id}`}>
                 {new Intl.NumberFormat("us-Us", {
                   style: "currency",
@@ -77,24 +91,24 @@ const Table = ({ onSort, filters, onFilter, items, onBuy }) => {
                 }).format(token.volume)}
               </Link>
             </td>
-            <td>
+            <td style={{ backgroundColor: backGround(token) }} className={styles.td}>
               <Link to={`/projects/${token.id}`}>
                 {token.roi}
                 {` %`}
               </Link>
             </td>
-            <td>
+            <td style={{ backgroundColor: backGround(token) }} className={styles.td}>
               <Link to={`/projects/${token.id}`}>{token.free}</Link>
             </td>
-            <td>
+            <td style={{ backgroundColor: backGround(token) }} className={styles.td}>
               <Link to={`/projects/${token.id}`}>
                 {token.hedge}
                 {` %`}
               </Link>
             </td>
-            <td>
-              <button onClick={e => BuyToken(e)} value={token.id}>
-                buy
+            <td style={{ backgroundColor: backGround(token) }} className={styles.td}>
+              <button className={styles.button} onClick={e => BuyToken(e)} value={token.id}>
+                Buy
               </button>
             </td>
           </tr>
